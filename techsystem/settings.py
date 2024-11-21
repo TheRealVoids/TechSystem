242f4c52518c5b7a8586b9da5f261c1b433668d4
Juan Manuel Marín Angarita <1126597295@u.icesi.edu.co>
2024-11-21 15:37:35 -0500

    feat: Added pgadmin connection and app

--- techsystem/settings.py
+++ techsystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.common.services.pgadmin",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "techsystem.wsgi.application"
 
 DATABASES = {
     "default": {
-        "  ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "PORT": 5432,
+        "NAME": "TechSystem",
+        "USER": "postgres",
+        "PASSWORD": "admin",
     }
 }
 
