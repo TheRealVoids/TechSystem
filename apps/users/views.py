52e26da3f58d1fec77b00ec7e8989aaaff3fe98c
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-11-25 16:06:34 -0500

    feat: add delivery certificates and equipments view, show user roles

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 from django.shortcuts import render
 
 from apps.common.services.pgadmin.models import UserAccount
@@ -7,4 +8,11 @@ from apps.common.services.pgadmin.models import UserAccount
 @login_required
 def show_users(request):
     users = UserAccount.objects.all()
+    for user in users:
+        roles = user.userrole_set.select_related("role_id").values(
+            _id=F("role_id__role_id"),
+            role_name=F("role_id__role_name"),
+            description=F("role_id__description"),
+        )
+        user.roles = list(roles)
     return render(request, "layouts/users.html", {"users": users})
