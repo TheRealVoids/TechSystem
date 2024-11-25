ee348e3dd468dc9d6d78a8c857206dd7e2bd2ce9
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-11-25 18:35:12 -0500

    feat: Adding widgets to home page

--- techsystem/views.py
+++ techsystem/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from apps.contracts.models import Contract
+
 
 def index(request):
-    return render(request, "index.html")
+    last_contract = (
+        Contract.objects.filter(user=request.user).order_by("-start_date").first()
+    )  # Obtén el último contrato del usuario autenticado
+    context = {
+        "last_contract": last_contract,
+    }
+    return render(request, "index.html", context)
