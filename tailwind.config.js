f375002fca8d459f098907403f59031afa791881
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-11-25 03:11:32 -0500

    feat: Changing base.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html",
+    // Templates within theme app (e.g. base.html)
+    './templates/**/*.html',
+    // Templates in other apps
+    './**/templates/**/*.html',
+    // Include JavaScript files if you use Tailwind classes in JS
+    './static/src/**/*.js',
+    // Exclude node_modules
+    '!./node_modules',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
