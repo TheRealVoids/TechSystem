e237d71e41ce3b95038ec9ab843204bf6af33d25
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-11-25 13:09:36 -0500

    feat: Log in page done

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -3,5 +3,18 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                "class": "text-white bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-white",
+            }
+        ),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "text-white bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-white",
+            }
+        )
+    )
