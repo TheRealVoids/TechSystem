fd7f2dd5d3018340d48291b7a883ae3b6de296a7
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-11-25 16:57:09 -0500

    feat: show opportunity history

--- apps/opportunities/urls.py
+++ apps/opportunities/urls.py
@@ -6,4 +6,9 @@ app_name = "opportunities"
 
 urlpatterns = [
     path("", views.show_opportunities, name="show_opportunities"),
+    path(
+        "<str:opportunity_id>/history/",
+        views.show_opportunity_history,
+        name="show_opportunity_history",
+    ),
 ]
--- apps/opportunities/views.py
+++ apps/opportunities/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
-from apps.common.services.pgadmin.models import Opportunity
+from apps.common.services.pgadmin.models import Opportunity, OpportunityStageHistory
 
 
 def show_opportunities(request):
     opportunities = Opportunity.objects.all()
     return render(
         request, "layouts/opportunities.html", {"opportunities": opportunities}
+    )
+
+
+def show_opportunity_history(request, opportunity_id):
+    opportunity = Opportunity.objects.get(opportunity_id=opportunity_id)
+    history = OpportunityStageHistory.objects.filter(
+        opportunity_id=opportunity_id
+    ).order_by("change_date")
+    return render(
+        request,
+        "layouts/opportunity_history.html",
+        {"opportunity": opportunity, "history": history},
     )
