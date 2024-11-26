b485feed657e0c8e38a7ef40dddd84b4b966cd14
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-11-26 14:05:56 -0500

    chores: add mongo default connection

--- apps/leases/views.py
+++ apps/leases/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from apps.common.services.mongodb.models import RentalRequests
+
 
 @login_required
 def show_leases(request):
-    return render(request, "layouts/leases.html")
+    leases = RentalRequests.objects.all()
+    return render(request, "layouts/leases.html", {"leases": leases})
